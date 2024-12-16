6c40816e102a1c90878b5cfdaa5cc6149a24692c
BrotherMo1 <155409498+BrotherMo1@users.noreply.github.com>
2024-12-16 09:48:22 -0800

    making installable

--- script.js
+++ script.js
@@ -13,6 +13,32 @@ window.onload = function() {
 } // window.onload
 
 
+// make maze installable
+// handle install prompt
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});     
+
+
 // get data from TV Maze
 async function searchTvShows() {
   document.getElementById("main").innerHTML = "";
